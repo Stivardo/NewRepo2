8f5bbe44807cc633ff64e9522121c45e837dd5f3
Roberth <Roberth@tal.com>
2024-09-29 20:43:37 -0500

    Cambio de fondo

--- MoansoPrueba/Form1.Designer.cs
+++ MoansoPrueba/Form1.Designer.cs
@@ -41,9 +41,10 @@
             // button1
             // 
             this.button1.BackColor = System.Drawing.SystemColors.ButtonShadow;
-            this.button1.Location = new System.Drawing.Point(50, 73);
+            this.button1.Location = new System.Drawing.Point(38, 59);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(177, 89);
+            this.button1.Size = new System.Drawing.Size(133, 72);
             this.button1.TabIndex = 0;
             this.button1.Text = "Boton1";
             this.button1.UseVisualStyleBackColor = false;
@@ -51,36 +52,40 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(50, 235);
+            this.button2.Location = new System.Drawing.Point(38, 191);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(177, 91);
+            this.button2.Size = new System.Drawing.Size(133, 74);
             this.button2.TabIndex = 2;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(546, 73);
+            this.button3.Location = new System.Drawing.Point(410, 59);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(177, 89);
+            this.button3.Size = new System.Drawing.Size(133, 72);
             this.button3.TabIndex = 3;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(546, 235);
+            this.button4.Location = new System.Drawing.Point(410, 191);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(177, 89);
+            this.button4.Size = new System.Drawing.Size(133, 72);
             this.button4.TabIndex = 4;
             this.button4.Text = "button4";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(254, 356);
+            this.button5.Location = new System.Drawing.Point(190, 289);
+            this.button5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(269, 63);
+            this.button5.Size = new System.Drawing.Size(202, 51);
             this.button5.TabIndex = 5;
             this.button5.Text = "NUEVOBOTON";
             this.button5.UseVisualStyleBackColor = true;
@@ -89,35 +94,39 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(282, 84);
+            this.label1.Location = new System.Drawing.Point(212, 68);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(222, 69);
+            this.label1.Size = new System.Drawing.Size(180, 55);
             this.label1.TabIndex = 7;
             this.label1.Text = "Prueba";
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(328, 246);
+            this.button6.Location = new System.Drawing.Point(246, 200);
+            this.button6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(155, 67);
+            this.button6.Size = new System.Drawing.Size(116, 54);
             this.button6.TabIndex = 8;
             this.button6.Text = "Cambio Boton";
             this.button6.UseVisualStyleBackColor = true;
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(254, 177);
+            this.button7.Location = new System.Drawing.Point(190, 144);
+            this.button7.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(114, 49);
+            this.button7.Size = new System.Drawing.Size(86, 40);
             this.button7.TabIndex = 9;
             this.button7.Text = "button71";
             this.button7.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.button7);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.label1);
@@ -126,6 +135,7 @@
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
