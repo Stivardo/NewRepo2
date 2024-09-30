48903635062fd74cb36e6af73ab2942e3633b376
anton <anton@DESKTOP-776A2JU>
2024-09-29 19:08:27 -0500

    cambio5

--- MoansoPrueba/Form1.Designer.cs
+++ MoansoPrueba/Form1.Designer.cs
@@ -29,34 +29,27 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
+            this.PRUEBA = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(127, 73);
+            this.button1.Location = new System.Drawing.Point(50, 73);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(177, 89);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(50, 2);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 16);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
-            // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(127, 235);
+            this.button2.Location = new System.Drawing.Point(50, 235);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(177, 91);
             this.button2.TabIndex = 2;
@@ -65,7 +58,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(448, 73);
+            this.button3.Location = new System.Drawing.Point(546, 73);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(177, 89);
             this.button3.TabIndex = 3;
@@ -74,7 +67,7 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(448, 235);
+            this.button4.Location = new System.Drawing.Point(546, 235);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(177, 89);
             this.button4.TabIndex = 4;
@@ -83,23 +76,57 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(279, 347);
+            this.button5.Location = new System.Drawing.Point(254, 356);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(269, 63);
             this.button5.TabIndex = 5;
             this.button5.Text = "NUEVOBOTON";
             this.button5.UseVisualStyleBackColor = true;
             // 
+            // PRUEBA
+            // 
+            this.PRUEBA.AutoSize = true;
+            this.PRUEBA.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PRUEBA.Location = new System.Drawing.Point(335, 82);
+            this.PRUEBA.Name = "PRUEBA";
+            this.PRUEBA.Size = new System.Drawing.Size(92, 25);
+            this.PRUEBA.TabIndex = 6;
+            this.PRUEBA.Text = "PRUEBA";
+            this.PRUEBA.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(317, 264);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 25);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "CONTROL";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(335, 185);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 25);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "RESET";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PRUEBA);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -111,11 +138,13 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label PRUEBA;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
