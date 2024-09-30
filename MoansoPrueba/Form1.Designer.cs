26a0a3e955cd8ccc80290707f666cc42ae303d27
WaRr <williamsromer159@gmail.com>
2024-09-29 19:52:34 -0500

    Eliminación del botón de prueba y cambio de color al boton1

--- MoansoPrueba/Form1.Designer.cs
+++ MoansoPrueba/Form1.Designer.cs
@@ -34,17 +34,18 @@
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.button6 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonShadow;
             this.button1.Location = new System.Drawing.Point(50, 73);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(177, 89);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Text = "Boton1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -92,23 +93,11 @@
             this.label1.TabIndex = 7;
             this.label1.Text = "Prueba";
             // 
-            // button6
-            // 
-            this.button6.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.button6.ForeColor = System.Drawing.SystemColors.ControlLight;
-            this.button6.Location = new System.Drawing.Point(267, 219);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(245, 106);
-            this.button6.TabIndex = 8;
-            this.button6.Text = "PRUEBA";
-            this.button6.UseVisualStyleBackColor = false;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button6);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
@@ -130,7 +119,6 @@
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button6;
     }
 }
 
--- MoansoPrueba/Form1.cs
+++ MoansoPrueba/Form1.cs
@@ -26,5 +26,10 @@ namespace MoansoPrueba
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
